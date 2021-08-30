e2af17cc6ef8df4836d2c58ec4d99734a71c97a9
biliangjing-tf <1710615975@qq.com>
2021-08-31 07:31:56 +0800

    feat:解决放大镜效果

--- src/index.ts
+++ src/index.ts
@@ -7,30 +7,68 @@ interface OptsType {
 export default class GlassImg {
   boxEle: HTMLElement;
   handEle: HTMLElement;
+  bigImgEle: HTMLElement;
+  imgEle: HTMLElement;
+  $imgWidth: number;
+  $imgHeight: number;
+  $scaleX: number;
+  $scaleY: number;
 
   constructor(opts: OptsType) {
     this.init(opts);
   }
   // 初始化方法
   init(opts) {
+    const that = this;
     const boxEle = document.getElementById(opts.id);
     const handEle = document.createElement('div');
+    const bigImgEle = document.createElement('div');
+    const imgEle = document.createElement('img');
+    
+    imgEle.src = opts.img;
+    imgEle.style.position = 'absolute';
+    imgEle.style.top = 0 + 'px';
+    imgEle.style.left = 0 + 'px';
+    imgEle.onload = () => {
+      that.$imgWidth = imgEle.width;
+      that.$imgHeight = imgEle.height;
+      that.$scaleX = opts.width/imgEle.width;
+      that.$scaleY = opts.height/imgEle.height;
+
+      handEle.style.width = opts.width * that.$scaleX + 'px';
+      handEle.style.height = opts.height * that.$scaleY  + 'px';
+      handEle.style.position = 'absolute';
+      handEle.style.background = 'blue';
+      handEle.style.opacity = '0.4';
+      handEle.style.display = 'none';
+    };
+    
     boxEle.style.width = opts.width + 'px';
     boxEle.style.height = opts.height + 'px';
     boxEle.style.position = 'relative';
     boxEle.style.backgroundImage = `url(${opts.img})`;
     boxEle.style.backgroundSize = `cover`;
 
-    handEle.style.width = 200 + 'px';
-    handEle.style.height = 200 + 'px';
-    handEle.style.position = 'absolute';
-    handEle.style.background = 'blue';
-    handEle.style.opacity = '0.4';
+    
+    bigImgEle.style.width = opts.width + 'px';
+    bigImgEle.style.height = opts.height + 'px';
+    bigImgEle.style.position = 'absolute';
+    bigImgEle.style.overflow = 'hidden';
+    bigImgEle.style.left = boxEle.offsetLeft + opts.width + 'px';
+    bigImgEle.style.top = boxEle.offsetTop + 'px';
+    bigImgEle.style.display = 'none';
 
     boxEle.appendChild(handEle);
+    boxEle.appendChild(bigImgEle);
+
+    
+    bigImgEle.appendChild(imgEle);
 
     this.boxEle = boxEle;
     this.handEle = handEle;
+    this.bigImgEle = bigImgEle;
+    this.imgEle = imgEle;
+
     this.addEvent();
   }
 
@@ -40,6 +78,7 @@ export default class GlassImg {
       this.over();
     }
     this.boxEle.onmouseleave = () => {
+      console.log('levet');
       this.leave();
     }
     this.boxEle.onmousemove = (event) => {
@@ -50,14 +89,15 @@ export default class GlassImg {
 
   over() {
     this.handEle.style.display = 'block';
+    this.bigImgEle.style.display = 'block';
   }
   leave() {
-    // this.handEle.style.display = 'none';
+    this.handEle.style.display = 'none';
+    this.bigImgEle.style.display = 'none';
   }
   move(e) {
-    console.log(e);
-    let l = e.offsetX - this.handEle.offsetWidth/2;
-    let t = e.offsetY - this.handEle.offsetHeight/2;
+    let l = e.clientX - this.boxEle.offsetLeft - this.handEle.offsetWidth/2;
+    let t = e.clientY - this.boxEle.offsetTop - this.handEle.offsetHeight/2;
     if (l < 0) l = 0;
     if (t < 0) t = 0;
     if (l > this.boxEle.offsetWidth - this.handEle.offsetWidth) {
@@ -68,5 +108,7 @@ export default class GlassImg {
     };
     this.handEle.style.left = l + 'px';
     this.handEle.style.top = t + 'px';
+
+
   }
 }
